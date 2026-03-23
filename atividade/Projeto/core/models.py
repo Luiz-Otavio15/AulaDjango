0c620933ad941a17319cddc0eae9b6037febc5d3
Luiz-Otavio15 <oluiz9790@gmail.com>
2026-03-23 11:40:16 -0300

    up

--- atividade/Projeto/core/models.py
+++ atividade/Projeto/core/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Filme(models.Model):
     titulo = models.CharField(max_length=200)
-    descricao = models.CharField(max_length=100)
+    descricao = models.CharField()
     data = models.IntegerField()
     imagem = models.ImageField(upload_to='filmes/')
     
